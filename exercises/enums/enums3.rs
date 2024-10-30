fa50c34e46c624441b716c02c00cf2257985d382
hypochlorous <y58432551@163.com>
2024-10-30 14:33:36 +0800

    first commit

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -5,10 +5,14 @@
 // Execute `rustlings hint enums3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 enum Message {
     // TODO: implement the message variant types based on their usage below
+    Move(Point),
+    Echo(String),
+    ChangeColor((u8, u8, u8)),
+    Quit
+
 }
 
 struct Point {
@@ -22,7 +26,7 @@ struct State {
     quit: bool,
     message: String
 }
-
+   
 impl State {
     fn change_color(&mut self, color: (u8, u8, u8)) {
         self.color = color;
@@ -39,6 +43,12 @@ impl State {
     }
 
     fn process(&mut self, message: Message) {
+        match message{
+        Message::ChangeColor(t)         =>  self.change_color(t),
+        Message::Echo(msg)              => self.echo(msg),
+        Message::Move(point)            => self.move_position(point),
+        Message::Quit                   => self.quit(), 
+        }
         // TODO: create a match expression to process the different message
         // variants
         // Remember: When passing a tuple as a function argument, you'll need
@@ -58,7 +68,7 @@ mod tests {
             color: (0, 0, 0),
             message: "hello world".to_string(),
         };
-        state.process(Message::ChangeColor(255, 0, 255));
+        state.process(Message::ChangeColor((255, 0, 255)));
         state.process(Message::Echo(String::from("hello world")));
         state.process(Message::Move(Point { x: 10, y: 15 }));
         state.process(Message::Quit);
