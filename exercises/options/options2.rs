f0046ea4f8a923d892d12443ec8cbbc133500911
hypochlorous <y58432551@163.com>
2024-10-30 15:23:52 +0800

    update

--- exercises/options/options2.rs
+++ exercises/options/options2.rs
@@ -13,9 +13,10 @@ mod tests {
         let optional_target = Some(target);
 
         // TODO: Make this an if let statement whose value is "Some" type
-        word = optional_target {
-            assert_eq!(word, target);
+        if let Some(word) = optional_target {
+        	assert_eq!(word, target);
         }
+        
     }
 
     #[test]
@@ -32,6 +33,10 @@ mod tests {
         // TODO: make this a while let statement - remember that vector.pop also
         // adds another layer of Option<T>. You can stack `Option<T>`s into
         // while let and if let.
+        while let Some(Some(integer)) = optional_integers.pop() {
+            assert_eq!(integer, range);
+            range -= 1;
+        }
         integer = optional_integers.pop() {
             assert_eq!(integer, cursor);
             cursor -= 1;
